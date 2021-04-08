965a1d0456ea220e0a6cfda62bcd58f2b91bda9e
Jonathan Truong <jjtruong90@gmail.com>
2021-04-07 21:37:58 -0700

    Update Filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList'
 
 function App() {
+
   const [inputText, setInputText] = useState("")
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+  
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status])
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+        default:
+          setFilteredTodos(todos);
+          break;
+    }
+  }
   return (
     <div className="App">
       <header>
@@ -17,8 +37,10 @@ function App() {
       todos={todos} 
       setTodos={setTodos} 
       inputText = {inputText}
-      setInputText={setInputText}></Form>
-      <TodoList setTodos={setTodos} todos={todos}></TodoList>
+      setInputText={setInputText}
+      setStatus={setStatus}
+      filteredTodos={filteredTodos}></Form>
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos}></TodoList>
       
 
     </div>
